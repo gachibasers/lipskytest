c671e480edf3fe0dda8b7d61700315a6c6c26219
gachibasers <gachibasers@users.noreply.github.com>
2025-12-02 21:22:03 +0000

    deploy: 01e09d9c5f425fc2e4ec338466918a6eaa13dbdd

--- _analyzer_8hpp.js
+++ _analyzer_8hpp.js
@@ -1,6 +1,5 @@
 var _analyzer_8hpp =
 [
     [ "BattleSimulator&lt; T &gt;", "class_battle_simulator.html", null ],
-    [ "LoreAI", "class_lore_a_i.html", "class_lore_a_i" ],
-    [ "LoreAI::GameInfo", "struct_lore_a_i_1_1_game_info.html", "struct_lore_a_i_1_1_game_info" ]
+    [ "findTankiest", "_analyzer_8hpp.html#af26756f0a0b4b47c76752d79229e0573", null ]
 ];
